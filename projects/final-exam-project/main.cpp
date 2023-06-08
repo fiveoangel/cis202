ae12d750e51b5011edd25bb34690d0605d2cb458
fiveoangel <129228982+fiveoangel@users.noreply.github.com>
2023-06-08 22:37:19 +0000

    Third Commit

--- projects/final-exam-project/main.cpp
+++ projects/final-exam-project/main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 class Mountain {
     public:
     void setName(string mountain);
+    string getName();
     void setCountry(string place);
+    string getCountry();
     void setElevation(int feet);
+    int getElevation();
     int toMeters();
 
     private:
@@ -20,21 +24,36 @@ void Mountain::setName(string mountain){
     name = mountain;
 }
 
+string Mountain::getName() {
+    return name;
+}
+
 void Mountain::setCountry(string place){
     country = place;
+}
+
+string Mountain::getCountry() {
+    return country;
 }
 
 void Mountain::setElevation(int feet){
     elevation = feet;
 }
 
-int Mountain::toMeters(){
+int Mountain::getElevation() {
+    return elevation;
+}
+
+int Mountain::toMeters() {
     meters = elevation/3.2808;
     return meters;
 }
 
 int main() {
-
+unsigned int i;
+int minElevation;
+vector<Mountain> mountainList;
+//MOUNTAINS
 Mountain Chimborazo;
 Mountain Matterhorn;
 Mountain Olympus;
@@ -43,5 +62,47 @@ Mountain Adirondacks;
 Mountain Mitchell;
 Mountain Zugspitze;
 
+//SET NAMES
+Chimborazo.setName("Chimborazo");
+Matterhorn.setName("Matterhorn");
+Olympus.setName("Olympus");
+Everest.setName("Everest");
+Adirondacks.setName("Mount Marcy - Adirondacks");
+Mitchell.setName("Mount Mitchell - Blue Ridge");
+Zugspitze.setName("Zugspitze");
+
+//SET COUNTRIES
+Chimborazo.setCountry("Ecuador");
+Matterhorn.setCountry("Switzerland");
+Olympus.setCountry("Greece (Macedonia)");
+Everest.setCountry("Nepal");
+Adirondacks.setCountry("United States");
+Mitchell.setCountry("United States");
+Zugspitze.setCountry("Switzerland");
+
+//SET ELEVATIONS
+Chimborazo.setElevation(20549);
+Matterhorn.setElevation(14692);
+Olympus.setElevation(9573);
+Everest.setElevation(29029);
+Adirondacks.setElevation(5344);
+Mitchell.setElevation(6684);
+Zugspitze.setElevation(9719);
+
+//ADD TO VECTOR
+mountainList.push_back(Chimborazo);
+mountainList.push_back(Matterhorn);
+mountainList.push_back(Olympus);
+mountainList.push_back(Everest);
+mountainList.push_back(Adirondacks);
+mountainList.push_back(Mitchell);
+mountainList.push_back(Zugspitze);
+
+minElevation = 0;
+for(i=0; i < mountainList.size(); i++) {
+    if(mountainList.at(i).getElevation() > minElevation) {
+        minElevation = mountainList.at(i);
+    }
+}
 
 }
